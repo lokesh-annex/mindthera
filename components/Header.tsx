f04226054ed425e953e429ffa51a8d50197b15d7
lokesh-annex <lokesh@annexlogics.com>
2025-07-28 16:55:27 +0530

    homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,58 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 export default function Header() {
   return (
   <header className="transparent scroll-light has-topbar">
-        <div id="topbar">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="d-flex justify-content-between xs-hide">
-                  {/* Left Topbar Widgets */}
-                  <div className="header-widget d-flex">
-                    <div className="topbar-widget">
-                      <a href="#">
-                        <i className="icofont-location-pin"></i>100 S Main St,
-                        Los Angeles, CA
-                      </a>
-                    </div>
-                    <div className="topbar-widget">
-                      <a href="#">
-                        <i className="icofont-clock-time"></i>
-                        <span>Mon - Sat: 8AM - 9PM</span>
-                        <span className="ms-3">Sunday: 10AM - 8PM</span>
-                      </a>
-                    </div>
-                    <div className="topbar-widget">
-                      <a href="#">
-                        <i className="icofont-envelope"></i>
-                        contact@mindthera.com
-                      </a>
-                    </div>
-                  </div>
-
-                  {/* Right Social Icons */}
-                  <div className="social-icons">
-                    <a href="#">
-                      <i className="fa-brands fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fa-brands fa-x-twitter fa-lg"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fa-brands fa-youtube fa-lg"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fa-brands fa-pinterest fa-lg"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fa-brands fa-instagram fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="clearfix"></div>
-          </div>
-        </div>
+      
 
         {/* Main Header */}
         <div className="container">
