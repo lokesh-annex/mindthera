ccd686739f2735981f25991a2e6379080f48a955
lokesh-annex <lokesh@annexlogics.com>
2025-08-20 15:11:50 +0530

    homepage changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ const ContactPage = () => (
   <>
    <Breadcrumbs
         title="Contact"
-        items={[{ label: "Home", href: "/" }, { label: "Contact" }]}
+        items={[{ label: "Kontakt", href: "/" }, { label: "Kontakt" }]}
       />
   <section>
     <div className="container">
@@ -58,7 +58,14 @@ const ContactPage = () => (
                 <input type="text" name="phone" id="phone" className="form-control no-border" placeholder="Your Phone" required />
               </div>
               <div className="field-set mb20">
-                <textarea name="message" id="message" className="form-control no-border" placeholder="Your Message" required></textarea>
+                <textarea
+                  name="message"
+                  id="message"
+                  className="form-control no-border"
+                  placeholder="Your Message"
+                  required
+                  rows={5}
+                />
               </div>
               
               <div id="submit" className="mt20">
